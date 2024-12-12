48c740486be06129a1e5e928552644688f11e397
TitusBilutaViorel <titusbiluta@gmail.com>
2024-12-12 13:56:33 +0200

    Update the workflow

---
+++ test.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const assert = require('assert');
+
+const options = {
+  hostname: 'localhost',
+  port: 3000,
+  path: '/',
+  method: 'GET',
+};
+
+const req = http.request(options, (res) => {
+  assert.strictEqual(res.statusCode, 200, 'Status code should be 200');
+  res.on('data', (chunk) => {
+    assert.strictEqual(chunk.toString(), 'Hello Titus, futere DevOps Engineer!\n', 'Response should be "Hello DevOps!"');
+  });
+});
+
+req.on('error', (error) => {
+  console.error(`Error: ${error.message}`);
+});
+
+req.end();
